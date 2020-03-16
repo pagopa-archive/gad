87bc432f78bec14a000c57247229f550f8be8148
Francesco Persico <francescopersico@gmail.com>
2020-03-16 17:31:23 +0100

    Do not load body-parser middleware

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,3 @@
-import bodyParser from "body-parser";
 import dotenv from "dotenv";
 import express from "express";
 import { createProxyMiddleware } from "http-proxy-middleware";
@@ -32,10 +31,6 @@ function roundrobin(ips: ReadonlyArray<string>): () => string {
 }
 
 const app = express();
-
-// Configuring body parser middleware
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
 
 app.use(
   requireClientCertificate(
